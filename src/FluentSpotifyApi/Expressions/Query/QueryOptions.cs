86e6ec53c06e973a6f650599f54d442865ab1090
Patrik Gebrian <patrik.gebrian@gmail.com>
2017-10-15 20:16:41 +0200

    Added XML doc

--- src/FluentSpotifyApi/Expressions/Query/QueryOptions.cs
+++ src/FluentSpotifyApi/Expressions/Query/QueryOptions.cs
@@ -5,8 +5,18 @@
     /// </summary>
     public class QueryOptions
     {
+        /// <summary>
+        /// If set to <c>true</c>, removes ["] characters from exact match and [":] characters from partial match.
+        /// Set to <c>false</c> by default.
+        /// </summary>
         public bool RemoveSpecialCharacters { get; set; }
 
+        /// <summary>
+        /// If set to <c>true</c>, converts OR and NOT words to lowercase and distributes field name and NOT operator
+        /// in partial match. (e.g. <c>f => f.Artist.Contains("NOT") &amp;&amp; !f.Artist.Contains("nice so*")</c> 
+        /// will be converted into "artist:not NOT artist:nice NOT artist:so*")
+        /// Set to <c>false</c> by default.
+        /// </summary>
         public bool NormalizePartialMatch { get; set; }
     }
 }
